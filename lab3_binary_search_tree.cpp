4cd3db66f3b2ef844b66ccc3def0f3edf0a8daa8
nancyw2774 <59981490+nancyw2774@users.noreply.github.com>
2020-03-30 17:08:31 -0400

    Update lab3_binary_search_tree.cpp

--- lab3_binary_search_tree.cpp
+++ lab3_binary_search_tree.cpp
@@ -5,10 +5,24 @@ using namespace std;
 
 // PURPOSE: Default/empty constructor
 BinarySearchTree::BinarySearchTree() {
+	root = NULL;
+	size = 0;
+}
+
+void BinarySearchTree::destroy(TaskItem* node){ //ADD TO HEADER
+	if (node)
+	{
+		destroy(node->left);
+		destroy(node->right);
+		delete node;
+	}
 }
 
 // PURPOSE: Explicit destructor of the class BinarySearchTree
 BinarySearchTree::~BinarySearchTree() {
+	destroy(root);
+	root = NULL;
+	size = 0;
 }
 
 // PURPOSE: Returns the number of nodes in the tree
@@ -25,7 +39,14 @@ BinarySearchTree::TaskItem BinarySearchTree::max() const {
 // PURPOSE: Returns the minimum value of a node in the tree
 // if the tree is empty, it returns (-1, "N/A")
 BinarySearchTree::TaskItem BinarySearchTree::min() const {
-	return BinarySearchTree::TaskItem(-1, "N/A");
+	if (!root)
+		return BinarySearchTree::TaskItem(-1, "N/A");
+	TaskItem* cur = root;
+	if (size == 1)
+		return *cur;
+	while ((cur->left))
+		cur = cur->left;
+	return *cur;
 }
 
 // PURPOSE: Returns the tree height
@@ -45,7 +66,7 @@ bool BinarySearchTree::exists( BinarySearchTree::TaskItem val ) const {
 
 // PURPOSE: Optional helper function that returns a pointer to the root node
 BinarySearchTree::TaskItem* BinarySearchTree::get_root_node() {
-    return NULL;
+    return root;
 }
 
 // PURPOSE: Optional helper function that returns the root node pointer address
@@ -67,5 +88,73 @@ bool BinarySearchTree::insert( BinarySearchTree::TaskItem val ) {
 // PURPOSE: Removes the node with the value val from the tree
 // returns true if successful; returns false otherwise
 bool BinarySearchTree::remove( BinarySearchTree::TaskItem val ) {
-    return false;
+    if (!root)
+		return false;
+	if (root->priority == val.priority && size == 1) {//if one element
+		delete root;
+		root = NULL;
+		size--;
+		return true;
+	}
+
+	TaskItem* cur = root;
+	TaskItem* prev = root;
+	bool found = false;
+	//finding node to remove
+	while (!found) {
+		if (cur->priority > val.priority && cur->left != NULL) {
+			if (cur->left->priority == val.priority){//stops if next one is node
+				prev = cur;
+				found = true;
+			}
+			cur = cur->left;
+		}
+		else if (cur->priority < val.priority && cur->right != NULL) {
+			if (cur->right->priority == val.priority) {//ditto
+				prev = cur;
+				found = true;
+			}
+			cur = cur->right;
+		}
+		else
+			return false;
+	}
+
+	//removing node
+	while (cur->right != NULL || cur->left != NULL){//if not leaf node
+		TaskItem* temp = cur;
+		prev = cur;
+
+		if (cur->left != NULL) {//moving successor up
+			temp = temp->left;
+			while (temp->right != NULL) {
+				if (temp->right->right == NULL)
+					prev = temp;
+				temp = temp->right;
+			}
+		}
+		else {//moving predecessor up
+			temp = temp->right;
+			while (temp->left != NULL) {//>1 child
+				if (temp->left->left == NULL)
+					prev = temp;
+				temp = temp->left;
+			}
+		}
+		*cur = *temp;
+		cur = temp;
+		*cur = val;
+	}
+
+	//deleting extra node
+	if (prev->left == cur) {
+		delete prev->left;
+		prev->left = NULL;
+	}
+	else {
+		delete prev->right;
+		prev->right = NULL;
+	}
+	size--;
+	return true;
 }
